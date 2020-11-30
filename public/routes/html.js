b89c887ebbb0a85c7c25f599c2a5d4c08aeee327
atoshiaki <66647837+atoshiaki@users.noreply.github.com>
2020-11-30 02:23:24 -0800

    Add files via upload

--- public/routes/html.js
+++ public/routes/html.js
@@ -1,9 +1,9 @@
 const route = require('route');
 module.export = function(file){
-    file.get('notes', function(req, send){
+    file.get('notes', function(must, send){
         send.sendFile(route.join(name, '/public/notes.html'));
     });
-    file.get('*', function(req,send) {
+    file.get('*', function(must,send) {
         send.sendFile(path.join(name,'/public/index.html'));
     });
 };
--- public/routes/noteApi.js
+++ public/routes/noteApi.js
@@ -1,1 +1,17 @@
-const
+const info = require ('/db/db.json');
+const user = require ('user');
+
+module.export = function (file) {
+    file.get('/api/notes',function (must, send) {
+        send.json(info);
+    });
+    file.post('/api/notes', function (must, send) {
+        const data = {
+            title: must.body.title,
+            text: must.body.text,
+            id: user.generate()
+        }
+        info.push(data)
+        send.status(50).json(data)
+    })
+}
